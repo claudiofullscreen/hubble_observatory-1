457201ff53fb1b99beec4591101f7f5e68ce6d06
Bruce Park <bruce.park@fullscreen.net>
2017-05-04 14:22:52 -0700

    Serialize request body params for POST
    Adhere to JSON API spec for a POST request
    Update method names

--- lib/hubble_api_client/request.rb
+++ lib/hubble_api_client/request.rb
@@ -1,18 +1,28 @@
 module HubbleApiClient
   class Request
-    def self.get_hubble_uuid(email)
-      parse Connection::API.get_request(route: "hubble_uuid", query: build_query({"email" => email}))
+    def self.get_uuid(email)
+      parse Connection::API.get_request(route: "accounts-users", query: build_query({"email" => email}))
     end
 
-    def self.create_hubble_uuid(email)
-      parse Connection::API.post_request(route: "hubble_uuid", body: build_query({"email" => email}))
-    end
-
-    def build_query(params)
-      params.map { |k, v| "#{k}=#{v}" }.join("&")
+    def self.create_uuid(email)
+      body = serialize_attributes(attributes: {email: email}, resource_type: "accounts_users")
+      parse Connection::API.post_request(route: "accounts-users", body: body)
     end
 
     private
+
+    def self.build_query(params)
+      params.map { |k, v| "#{k}=#{v}" }.join("&")
+    end
+
+    def self.serialize_attributes(attributes:, resource_type:)
+      {
+        data: {
+          type: resource_type,
+          attributes: attributes
+        }
+      }
+    end
 
     def self.parse(response)
       if response.code == '200'
