bb1565eb69c51667270ca806e1282eaa8b447eb8
Bruce Park <bruce.park@fullscreen.net>
2017-05-12 17:11:01 -0700

    API contract should return hubble id or nil
    Right now tests are passing only if:
    
    bundle exec rspec spec/talent_accounts/talent_account_spec.rb

--- lib/hubble_api_client/configuration.rb
+++ lib/hubble_api_client/configuration.rb
@@ -2,9 +2,9 @@ module HubbleApiClient
   class Configuration
     attr_accessor :app_access_token, :host_url
 
-    def initialize
+    def initialize(environment: "staging")
       @app_access_token = ENV['HUBBLE_APP_TOKEN']
-      @host_url = ENV['HUBBLE_API_URL']
+      @host_url = environment == "production" ? "production-hubble.fullscreen.net" : "stage-hubble.fullscreen.net"
     end
   end
 end
--- spec/talent_accounts/talent_account_spec.rb
+++ spec/talent_accounts/talent_account_spec.rb
@@ -1,38 +1,38 @@
 require "spec_helper"
 
 RSpec.describe HubbleApiClient::TalentAccount do
-  let(:valid_email) { "brian.lu@fullscreen.com" }
+  let(:valid_email) { "user@example.com" }
   let(:invalid_email) { "john-example.com" }
-  let(:hubble_uuid) { "1234" }
+  let(:hubble_uuid) { "3650735786582309797" }
 
   describe "create" do
     context "with valid arguments" do
-      let(:talent_account) { HubbleApiClient::TalentAccount.create(email: valid_email) }
-      it "should return a TalentAccount" do
-        expect(talent_account.hubble_uuid).to eq hubble_uuid
+      let(:talent_account_creation) { HubbleApiClient::TalentAccount.create(email: valid_email) }
+      it "should return a TalentAccount hubble id" do
+        expect(talent_account_creation).to eq hubble_uuid
       end
     end
 
     context "with invalid arguments" do
-      let(:talent_account) { HubbleApiClient::TalentAccount.create(email: invalid_email) }
+      let(:talent_account_creation) { HubbleApiClient::TalentAccount.create(email: invalid_email) }
       it "should return nil" do
-        expect(talent_account.hubble_uuid).to eq(nil)
+        expect(talent_account_creation).to be_nil
       end
     end
   end
 
   describe "update" do
     context "with valid arguments" do
-      let(:talent_account) { HubbleApiClient::TalentAccount.update(attributes: {email: valid_email}, hubble_uuid: hubble_uuid) }
-      it "should return a TalentAccount" do
-        expect(talent_account.hubble_uuid).to eq hubble_uuid
+      let(:talent_account_update) { HubbleApiClient::TalentAccount.update(attributes: {email: valid_email}, hubble_uuid: hubble_uuid) }
+      it "should return a TalentAccount hubble id" do
+        expect(talent_account_update).to eq hubble_uuid
       end
     end
 
     context "with invalid arguments" do
-      let(:talent_account) { HubbleApiClient::TalentAccount.update(attributes: {email: invalid_email}, hubble_uuid: hubble_uuid) }
-      it "should raise an error" do
-        expect(talent_account.hubble_uuid).to eq(nil)
+      let(:talent_account_update) { HubbleApiClient::TalentAccount.update(attributes: {email: invalid_email}, hubble_uuid: hubble_uuid) }
+      it "should be nil" do
+        expect(talent_account_update).to be_nil
       end
     end
   end
